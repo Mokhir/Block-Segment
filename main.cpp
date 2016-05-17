4debd8fe0b50b4abd24e4d0cd74c5443afc68ba3
Waleed Qawasmi <wqawasmi@gmail.com>
2016-05-17 02:14:39 -0400

    Now must pass file to hash when calling program. Added testcase test.txt

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 #include "segment.h"
 
 int main(int argc, char **argv) {
+  if( argc < 2 ) {
+    std::cout << "Not enough arguments.\n" << argv[0] << " <file>" << std::endl;
+    return -1;
+  }
+
   std::cout << "Running.\n" << std::endl;
 
   /*
@@ -17,7 +22,7 @@ int main(int argc, char **argv) {
   getMD5Hash((byte *)message.c_str(), message.length(), digest);
   */
 
-  std::ifstream file ("test.txt", std::ios::binary);
+  std::ifstream file (argv[1], std::ios::binary);
 
   // Request block of memory within range of file
   char *block = (char*)malloc(sizeof(char)*10);
